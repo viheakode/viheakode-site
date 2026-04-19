7c94829fec99bfad9526aa74374e5960bbb2b58a
viheakode <viheakode787@gmail.com>
2026-04-19 16:38:39 +0700

    Working on send message

--- js/script.js
+++ js/script.js
@@ -1,6 +1,25 @@
 const API_URL = "https://dash.viheakode.online"
 const API_TOKEN = "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJVU0VSIiwiaWF0IjoxNzc2NTg2NTY3LCJleHAiOjE4MDgxMjI1Njd9.NYo8v4193WpZ-qqZE0lGxmagZDm2X6bU9vl8yaObSKQ"
 
+toastr.options = {
+  "closeButton": false,
+  "debug": false,
+  "newestOnTop": false,
+  "progressBar": false,
+  "positionClass": "toast-top-right",
+  "preventDuplicates": false,
+  "onclick": null,
+  "showDuration": "300",
+  "hideDuration": "1000",
+  "timeOut": "2000",
+  "extendedTimeOut": "1000",
+  "showEasing": "swing",
+  "hideEasing": "linear",
+  "showMethod": "fadeIn",
+  "hideMethod": "fadeOut"
+}
+
+
 async function sendMessage(data){
     const resp = await fetch(`${API_URL}/api/v1/messages`, {
         method: 'POST',
@@ -11,7 +30,10 @@ async function sendMessage(data){
         body: JSON.stringify(data)
     })
     const result = await resp.json();
-    console.log(result)
+    toastr.success("Message sent.");
+    setTimeout(() => {
+        window.location.href = "/";
+    }, 2000);
 
 }
 
